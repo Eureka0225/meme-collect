a88ee07cab3daa37dbc9b2ca506c20e99d2b9fb9
waterblock79 <33573572+waterblock79@users.noreply.github.com>
2020-08-27 17:10:41 +0800

    Add files via upload
    Frist Commit

---
+++ js/getall.js
@@ -0,0 +1,21 @@
+var req=new XMLHttpRequest();
+var res={};
+var i;
+req.open("GET",'resources/all.json',false);
+req.send();
+res=JSON.parse(req.responseText);
+for(i=0;i<res.length;i++)
+{
+   document.write("<a href='"+"bq.html?id="+i+"'><div class='card' style='background:url(\"resources/"+res[i].img+"\");background-repeat:no-repeat;background-size:100% 100%;'>");
+   document.write("<div class='card_about'><center>"+sub(res[i].name)+"</center></div>");
+   document.write("</div></a>");
+}
+function sub(n)
+{
+   if(n.length>8) {
+      return n.substring(0,7)+"...";
+   }
+   else {
+      return n;
+   }
+}
---
+++ js/getbq.js
@@ -0,0 +1,33 @@
+var req=new XMLHttpRequest();
+var res={};
+var i;
+req.open("GET",'resources/all.json',false);
+req.send();
+res=JSON.parse(req.responseText);
+document.write("<span class='top_title'>"+res[getQueryVariable("id")].name+"</span><br/>");
+document.write("<span class='top_about'>表情资源名称："+res[getQueryVariable("id")].img+"</span><br/>");
+document.write("<span class='top_about'>表情Tag："+getTag()+"</span><br/>");
+window.onload=function (){
+  document.getElementById("bqimg").src="resources/"+res[getQueryVariable("id")].img;
+};//因为这个js在<img>前面执行，所以执行时还没有<img>，所以页面需要完全加载后再设置img.src
+
+function getTag() {
+   var tags="";
+   for(var i=0;i<res[getQueryVariable("id")].tag.length;i++)
+   {
+      tags=tags+"<span style='line-height:200%;background:red;border-radius:2px;padding:0.1em 0.3em 0.1em 0.3em;'>"+res[getQueryVariable("id")].tag[i]+"</span>&nbsp;";
+   }
+   return tags;
+}
+
+//==以下代码来自Runoob.com/w3cnote/js-get-url-param.html==
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
